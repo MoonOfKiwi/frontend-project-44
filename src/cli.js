42f827d3a442243da926c37abfa2150b7fc25f3f
Anastasia_Surkova <existence117@gmail.com>
2024-01-24 18:33:08 +0300

    implemented sayYourName function

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const sayYourName = () => {
+    const nameMan = readlineSync.question('May I have your name? ')
+    console.log(`Hello, ${nameMan}!`)
+};
+
+export { sayYourName };
