e01f4b629f5ed677f3f7015f4294d7a87aa696b7
Anastasia_Surkova <existence117@gmail.com>
2024-02-01 16:18:14 +0300

    style: add function printRoundResult -> startGame; delete function printRoundResult

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import readlineSync from 'readline-sync';
 import askUsername from './cli.js';
 
-const printRoundResult = (isAnswerCorrect, username, userAnswer, correctAnswer) => {
-  if (isAnswerCorrect) {
-    console.log('Correct!');
-  } else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${username}!`);
-  }
-};
-
-const startGame = (getRules, getInfoForRound) => {
+const startGame = (Rules, getInfoForRound) => {
   const roundsCount = 3;
 
   console.log('Welcome to the Brain Games!');
   const username = askUsername();
-  console.log(getRules);
+  console.log(Rules);
 
   for (let currentAttempt = 1; currentAttempt <= roundsCount; currentAttempt += 1) {
     const [question, correctAnswer] = getInfoForRound();
@@ -22,9 +14,12 @@ const startGame = (getRules, getInfoForRound) => {
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
-    const isAnswerCorrect = (userAnswer === correctAnswer);
-    printRoundResult(isAnswerCorrect, username, userAnswer, correctAnswer);
-    if (!isAnswerCorrect) return;
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${username}!`);
+      return;
+    }
   }
   console.log(`Congratulations, ${username}!`);
 };
