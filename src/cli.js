1a8b66663ef8d66989aad2ae07b95524bbb67666
Anastasia_Surkova <existence117@gmail.com>
2024-02-02 14:49:51 +0300

    feat: added greeting and rename func

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
 import readlineSync from 'readline-sync';
 
-const askUsername = () => {
+const askUsernameAndGreet = () => {
+  console.log('Welcome to the Brain Games!');
   const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}!`);
   return username;
 };
 
-export default askUsername;
+export default askUsernameAndGreet;
